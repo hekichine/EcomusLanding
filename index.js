7789b46d840df8541be50d94f7c86eff41c40e27
hekichien <chienlx1005@gmail.com>
2023-10-27 14:45:07 +0700

    update packery

--- index.js
+++ index.js
@@ -69,37 +69,191 @@ const app = {
     $(".close-menu").on("click", function () {
       $(".hdt-menu_nav").removeClass("open");
     });
-    $(window).on('resize',function(){
-      if($(window).width() > 1023){
+    $(".overlay").on("click", function () {
+      $(".hdt-menu_nav").removeClass("open");
+    });
+    $(window).on("resize", function () {
+      if ($(window).width() > 1023) {
         $(".hdt-menu_nav").removeClass("open");
       }
-    })
+    });
   },
-  cursor:()=>{
-    const cursor = document.getElementById('cursor');
-    
-    document.addEventListener('mousemove',e =>{
+  cursor: () => {
+    const cursor = document.getElementById("cursor");
+
+    document.addEventListener("mousemove", (e) => {
       // console.log(e.clientX,e.clientY);
       cursor.style.top = e.clientY + `px`;
       cursor.style.left = e.clientX + `px`;
-    })
+    });
   },
-  filter:()=>{
-    let $grid = $('#isotope').isotope({
-      itemSelector: '.isotope-item',
-      layoutMode: 'fitRows',
+  cursor2: () => {
+    const cursorOuter = document.querySelector(".cursor--large");
+    const cursorInner = document.querySelector(".cursor--small");
+    const cursorTextContainerEl = document.querySelector(".cursor--text");
+    const cursorTextEl = cursorTextContainerEl.querySelector(".text");
+
+    const hoverItems = document.querySelectorAll(".cursor-hover-item");
+    const hoverEffectDuration = 0.3;
+    let isHovered = false;
+    let initialCursorHeight;
+
+    const cursorRotationDuration = 8;
+
+    let circleType = new CircleType(cursorTextEl);
+    circleType.radius(50);
+
+    setTimeout(() => {
+      initialCursorHeight =
+        circleType.container.style.getPropertyValue("height");
+      console.log(initialCursorHeight);
+    }, 50);
+
+    hoverItems.forEach((item) => {
+      item.addEventListener("pointerenter", handlePointerEnter);
+      item.addEventListener("pointerleave", handlePointerLeave);
+    });
+
+    let mouse = {
+      x: -100,
+      y: -100,
+    };
+
+    document.body.addEventListener("pointermove", updateCursorPosition);
+
+    function updateCursorPosition(e) {
+      mouse.x = e.clientX;
+      mouse.y = e.clientY;
+    }
+
+    function updateCursor() {
+      gsap.set([cursorInner, cursorTextContainerEl], {
+        x: mouse.x,
+        y: mouse.y,
+      });
+
+      gsap.to(cursorOuter, {
+        duration: 0.15,
+        x: mouse.x,
+        y: mouse.y,
+      });
+
+      if (!isHovered) {
+        gsap.to(cursorTextContainerEl, hoverEffectDuration * 0.5, {
+          opacity: 0,
+        });
+        gsap.set(cursorTextContainerEl, {
+          rotate: 0,
+        });
+      }
+
+      requestAnimationFrame(updateCursor);
+    }
+
+    updateCursor();
+
+    function handlePointerEnter(e) {
+      isHovered = true;
+
+      const target = e.currentTarget;
+      updateCursorText(target);
+
+      gsap.set([cursorTextContainerEl, cursorTextEl], {
+        height: initialCursorHeight,
+        width: initialCursorHeight,
+      });
+
+      gsap.fromTo(
+        cursorTextContainerEl,
+        {
+          rotate: 0,
+        },
+        {
+          duration: cursorRotationDuration,
+          rotate: 360,
+          ease: "none",
+          repeat: -1,
+        }
+      );
+
+      gsap.to(cursorInner, hoverEffectDuration, {
+        scale: 2,
+      });
+
+      gsap.fromTo(
+        cursorTextContainerEl,
+        hoverEffectDuration,
+        {
+          scale: 1.2,
+          opacity: 0,
+        },
+        {
+          delay: hoverEffectDuration * 0.75,
+          scale: 1,
+          opacity: 1,
+        }
+      );
+      gsap.to(cursorOuter, hoverEffectDuration, {
+        scale: 1.2,
+        opacity: 0,
+      });
+    }
+
+    function handlePointerLeave() {
+      isHovered = false;
+      gsap.to([cursorInner, cursorOuter], hoverEffectDuration, {
+        scale: 1,
+        opacity: 1,
+      });
+    }
+
+    function updateCursorText(textEl) {
+      const cursorTextRepeatTimes = textEl.getAttribute(
+        "data-cursor-text-repeat"
+      );
+      const cursorText = returnMultipleString(
+        textEl.getAttribute("data-cursor-text"),
+        cursorTextRepeatTimes
+      );
+
+      circleType.destroy();
+
+      cursorTextEl.innerHTML = cursorText;
+      circleType = new CircleType(cursorTextEl);
+    }
+
+    function returnMultipleString(string, count) {
+      let s = "";
+      for (let i = 0; i < count; i++) {
+        s += ` ${string} `;
+      }
+      return s;
+    }
+  },
+  filter: () => {
+    let $grid = $("#isotope").isotope({
+      itemSelector: ".isotope-item",
+      layoutMode: "fitRows",
       filter: "*",
-    })
-    $('[filter-tabs]').on('click','button',function(){
-      var filterValue = $( this ).attr('data-filter');
+    });
+    $("[filter-tabs]").on("click", "button", function () {
+      var filterValue = $(this).attr("data-filter");
       $grid.isotope({ filter: filterValue });
-    })
+    });
   },
-  tabs:()=>{
-    $('[filter-tabs]').on('click','button',function(){
-      $('[filter-tabs]').find('button.is-active').removeClass('is-active')
-      $(this).addClass('is-active');
-    })
+  tabs: () => {
+    $("[filter-tabs]").on("click", "button", function () {
+      $("[filter-tabs]").find("button.is-active").removeClass("is-active");
+      $(this).addClass("is-active");
+    });
+  },
+  empower_masonry: () => {
+
+    $('.empower_grid').isotope({
+      layoutMode: 'packery',
+      itemSelector: '.col'
+    });
+    
   },
   start: () => {
     console.log("App start ...");
@@ -107,9 +261,11 @@ const app = {
     app.header_change_bg();
     app.nav_on_mb();
     app.open_menu_mb();
-    app.cursor();
+    // app.cursor();
+    // app.cursor2();
     app.tabs();
     app.filter();
+    app.empower_masonry();
   },
 };
 

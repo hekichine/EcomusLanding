382d1ae6cd86496309763c38d259cb696d2cc6ac
hekichien <chienlx1005@gmail.com>
2023-10-31 10:21:25 +0700

    update

--- index.js
+++ index.js
@@ -248,10 +248,80 @@ const app = {
     });
   },
   empower_masonry: () => {
-    $('.empower_grid').isotope({
-      layoutMode: 'packery',
-      itemSelector: '.col'
+    $(".empower_grid").isotope({
+      layoutMode: "packery",
+      itemSelector: ".col",
     });
+  },
+  splider:()=>{
+    imagesLoaded( document.querySelector('.splide'), function( instance ) {
+      // Create the AutoScroll extension.
+      const AutoScroll = function ( Splide, Components ) {
+        const Track      = Components.Track;
+        const pxPerFrame = 3;
+    
+        let paused = true;
+        let page   = 0;
+    
+        return {
+          mount() {
+            this.update = this.update.bind( this );
+    
+            Splide.on( 'mouseenter', () => {
+              this.pause();
+            }, Splide.root );
+    
+            Splide.on( 'mouseleave', () => {
+              this.play();
+            }, Splide.root );
+          },
+    
+          // Start scroll after load.
+          mounted() {
+            setTimeout( this.play.bind( this ), 1000 );
+          },
+    
+          // Start scroll.
+          play() {
+            if ( paused ) {
+              paused = false;
+              Components.Elements.list.style.transition = '';
+              requestAnimationFrame( this.update );
+            }
+          },
+    
+          // Pause scroll.
+          pause() {
+            paused = true;
+          },
+    
+          // Update the slider position on every frame.
+          update() {
+            Track.translate( Track.position - pxPerFrame );
+            Track.shift();
+    
+            const currentPage = Track.toIndex( Track.position );
+    
+            if ( page !== currentPage ) {
+              this.onPageChanged( currentPage, page );
+              page = currentPage;
+            }
+    
+            if ( ! paused ) {
+              requestAnimationFrame( this.update );
+            }
+          },
+    
+          // Called when the page is changed.
+          onPageChanged( newPage, prevPage ) {
+            console.log("Splide slider " + prevPage + '->' + newPage );
+          }
+        };
+      };
+    
+      new Splide( '.splide' ).mount( { AutoScroll } );
+    } );
+    
   },
   start: () => {
     console.log("App start ...");
@@ -264,6 +334,7 @@ const app = {
     app.tabs();
     app.filter();
     app.empower_masonry();
+    app.splider();
   },
 };
 

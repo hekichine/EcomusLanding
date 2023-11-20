b5ac109c30272f3f18d7e96e783ef12ab1a74efb
hekichien <chienlx1005@gmail.com>
2023-11-20 16:17:47 +0700

    update

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ const app = {
     }
     // start glow
     glow();
-    if(window > 768){
+    if(window > 1149){
       document.addEventListener("mousemove", (e) => {
         mouse_on();
         clearTimeout(timer);
